5e9d7f7cad14a90b28ef90d056264e81262b7a2d
Diego Bollini <dib@adhoc.com.ar>
2022-08-02 17:37:30 -0300

    update commands definitions

--- rke_byadhoc.sh
+++ rke_byadhoc.sh
@@ -8,12 +8,12 @@ r2_help () {
     echo "R2, el nuevo comando para trabajar con Rancher2 y Kubernetes by Adhoc! 🚀"
     echo "=================="
     echo "🤖 Lista de comandos:"
-    echo "connect: Acceder con bash a una base agregando el nombre del deploy / pod. Uso: r2 connect test-adhoc-31-12-1"
+    echo "connect: Acceder con bash a una base. Uso: r2 connect test-adhoc-31-12-1"
     echo "logs: Para ver los logs activos de una base. Uso: r2 logs test-adhoc-31-12-1"
     echo "describe: Muestra detalles de un recurso o grupo. Uso: r2 describe cotesma"
-    echo "gcp: Devuelve la URL para acceder a la consola > Pod (métricas, logs). Uso: r2 gcp symmetria"
-    echo "reg: Devuelve la URL para acceder a los logs desde GCP > Logs históricos. Uso: r2 reg perfit"
-    echo "redeploy: Cierra y reinicia cada contenedor del pod, no hay downtime ni reinicia valores del workload. Uso: r2 redeploy test-demo-retail-22-07-1"
+    echo "gcp: Devuelve la URL para acceder a la consola de GCP (DevOps). Uso: r2 gcp symmetria"
+    echo "reg: Devuelve la URL para acceder a los logs históricos desde GCP. Uso: r2 reg perfit"
+    echo "redeploy: Apaga y reinicia cada contenedor del deployment, no hay downtime ni reinicia valores del workload (rolling restart). Uso: r2 redeploy test-demo-retail-22-07-1"
 }
 
 r2_connect () {
@@ -32,6 +32,7 @@ r2_gcp () {
     echo https://console.cloud.google.com/kubernetes/deployment/us-east1-b/adhocprod/$1/$1-adhoc-odoo/overview?project=nubeadhoc
 }
 
+# DevOps
 r2_clean () {
     kubectl get pods --all-namespaces | grep Terminated | while read namespace pod rest; do kubectl delete pod $pod -n $namespace; done
 }
